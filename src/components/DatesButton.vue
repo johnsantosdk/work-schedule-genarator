71e60f85d229be12374b0712156d78cc30fe0d24
Airton Vieira dos Santos <airton.santos3.1415@gmail.com>
2020-05-15 12:39:58 -0300

    60% do component concluido

--- src/components/DatesButton.vue
+++ src/components/DatesButton.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="row">
         <div class="col">     
-            <button class="btn-mes-anterior btn btn-outline-secondary" >{{ monthsTest[currentMonth-1][0] }}</button>
+            <button class="btn-mes-anterior btn btn-outline-secondary" >{{ previousMonth }}</button>
         </div>
         <!-- <div class="col div-align-content-center" >
             <input type="date" class="form-control input-date" v-model="fullDate"><button>Data</button>{{ fullDate }}
@@ -13,8 +13,8 @@
             </div>
         </div> -->
         <div class="col-sm-1 input-group">
-            <select  class="form-control" v-model="currentMonth"> 
-                <option  v-for="month in monthsTest" :key="month[0][3]" :value="month[2]">{{month[0]}}</option>
+            <select  class="form-control" v-model="currentMonth" > 
+                <option  v-for="month in months" :key="month[2]" :value="month[2]">{{month[0]}}</option>
             </select>
         </div>
         <div class="col-sm-1 input-group">
@@ -23,11 +23,13 @@
                 <option value="02">2021</option>
                 <option value="03">2022</option>
             </select>
-        </div>    
+        </div> 
+        {{  currentMonth === undefined ? ' nao existe' : '  existe' }}
+
         <div class="col">
-            <button class="btn-mes-posterior btn btn-outline-secondary" >{{ monthsTest[currentMonth+1][0] }}</button>
+            <button class="btn-mes-posterior btn btn-outline-secondary " >{{ nextMonth }}</button>
         </div>  
-  {{  monthsTest[0][0] + "----" + fullDate}}
+  
     </div>
 </template>
 
@@ -36,7 +38,7 @@ export default {
     data() {
         return {
             fullDate: (new Date()).toUTCString(),
-            monthsTest: [
+            months: [
                 ['janeiro', 'jan','0', '1'],
                 ['fevereiro', 'fev','1', '2'],
                 ['março', 'mar','2', '3'],
@@ -50,29 +52,39 @@ export default {
                 ['novembro', 'nov','10', '11'],
                 ['dezembro', 'dez','11', '12']
             ],
-            // months: {
-            //     janeiro:'0',
-            //     fevereiro:'1',
-            //     março:'2',
-            //     abril:'3',
-            //     maio:'4',
-            //     junho:'5',
-            //     julho:'6',
-            //     agosto:'7',
-            //     setembro:'8',
-            //     outubro:'9',
-            //     novembro:'10',
-            //     dezembro:'11'
-            //     },
-            currentMonth: new Date().getMonth(),
-            currentYear: new Date().getFullYear()
+            monthsTest: [
+                'janeiro',
+                'fevereiro',
+                'março',
+                'abril',
+                'maio',
+                'junho',
+                'julho',
+                'agosto',
+                'setembro',
+                'outubro',
+                'novembro',
+                'dezembro',
+            ],
+            currentMonth: Number(new Date().getMonth()),
+            updateMonth: '',
+            currentYear: new Date().getFullYear() 
         } 
+    },
+    computed: {
+        previousMonth() {
+            let p = Number(this.currentMonth) == 0 ? 11 : Number(this.currentMonth) - 1
+            return this.monthsTest[p]
+            
+        },
+        nextMonth() {
+            let n = Number(this.currentMonth) == 11 ? 0 : Number(this.currentMonth) + 1
+            return this.monthsTest[n]
+        }
     },
     methods: {
         setDate() {
             const dt = new Date(this.fullDate)
-            // const dtt = new Date("2020-02-12")
-            console.log('current date: ',this.currentMonth)
             const date = {
                 fullDate: dt,
                 day: dt.getDate() + 1,
@@ -80,12 +92,11 @@ export default {
                 year: dt.getFullYear()
             }
             this.$store.state.date = date
-            
-            console.log(date)
         }
+
     },
     created() {
-        console.log()
+        // console.log(this.currentMonth)
     }
 }
 </script>
